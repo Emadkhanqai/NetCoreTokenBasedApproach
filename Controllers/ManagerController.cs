42a24ee31d15faefe60b2cd1fd56f23842ebdeda
Emad Hassan Khan <emadkhanqai@gmail.com>
2024-02-19 00:21:19 +0500

    Added ROLE BASED Authorization on controllers

---
+++ Controllers/ManagerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreTokenBasedApproach.Data.Helpers;
+
+namespace NetCoreTokenBasedApproach.Controllers
+{
+    [Authorize(Roles = UserRoles.Manager)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("done manager");
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreTokenBasedApproach.Data.Helpers;
+
+namespace NetCoreTokenBasedApproach.Controllers
+{
+    [Authorize(Roles = UserRoles.Student)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public StudentController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("done student");
+        }
+    }
+}
